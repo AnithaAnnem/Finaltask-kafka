3edc7aab53a36e41fd447ce641a0357b10e78299
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-17 08:30:50 +0530

    Update KafkaDeployment.groovy

--- KafkaSharedLib/src/org/cloudninja/kafka/template/KafkaDeployment.groovy
+++ KafkaSharedLib/src/org/cloudninja/kafka/template/KafkaDeployment.groovy
@@ -7,62 +7,57 @@ class KafkaDeployment implements Serializable {
         this.steps = steps
     }
 
+    // ===== Git Checkout =====
     def checkoutCode(String repo, String branch) {
-        steps.stage('Checkout Code') {
-            steps.echo "Checking out Git repository..."
-            steps.checkout([$class: 'GitSCM',
-                            branches: [[name: "*/${branch}"]],
-                            userRemoteConfigs: [[url: repo]]])
-        }
+        steps.echo "Checking out Git repository..."
+        steps.checkout([$class: 'GitSCM',
+                        branches: [[name: "*/${branch}"]],
+                        userRemoteConfigs: [[url: repo]]
+        ])
     }
 
+    // ===== Ansible Lint =====
     def ansibleLint(String playbook) {
-        steps.stage('Ansible Lint') {
-            steps.echo "Running ansible-lint..."
-            steps.sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-lint ${playbook}
-            """
-        }
+        steps.echo "Running ansible-lint on playbook..."
+        steps.sh """
+            export ANSIBLE_HOST_KEY_CHECKING=False
+            ansible-lint ${playbook}
+        """
     }
 
+    // ===== Ansible Syntax Check =====
     def ansibleSyntaxCheck(String inventory, String playbook) {
-        steps.stage('Ansible Syntax Check') {
-            steps.echo "Checking playbook syntax..."
-            steps.sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i ${inventory} ${playbook} --syntax-check
-            """
-        }
+        steps.echo "Checking Ansible playbook syntax..."
+        steps.sh """
+            export ANSIBLE_HOST_KEY_CHECKING=False
+            ansible-playbook -i ${inventory} ${playbook} --syntax-check
+        """
     }
 
+    // ===== Ansible Dry Run =====
     def ansibleDryRun(String inventory, String playbook, String user, String key) {
-        steps.stage('Ansible Dry Run (Check Mode)') {
-            steps.echo "Performing dry run..."
-            steps.sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i ${inventory} ${playbook} --check -u ${user} --private-key ${key}
-            """
-        }
+        steps.echo "Performing dry run (check mode) on playbook..."
+        steps.sh """
+            export ANSIBLE_HOST_KEY_CHECKING=False
+            ansible-playbook -i ${inventory} ${playbook} --check -u ${user} --private-key ${key}
+        """
     }
 
+    // ===== Deploy Zookeeper =====
     def deployZookeeper(String inventory, String playbook, String user, String key) {
-        steps.stage('Deploy Zookeeper') {
-            steps.echo "Deploying Zookeeper cluster..."
-            steps.sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags zookeeper
-            """
-        }
+        steps.echo "Deploying Zookeeper cluster..."
+        steps.sh """
+            export ANSIBLE_HOST_KEY_CHECKING=False
+            ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags zookeeper
+        """
     }
 
+    // ===== Deploy Kafka =====
     def deployKafka(String inventory, String playbook, String user, String key) {
-        steps.stage('Deploy Kafka') {
-            steps.echo "Deploying Kafka cluster..."
-            steps.sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags kafka
-            """
-        }
+        steps.echo "Deploying Kafka cluster..."
+        steps.sh """
+            export ANSIBLE_HOST_KEY_CHECKING=False
+            ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags kafka
+        """
     }
 }
