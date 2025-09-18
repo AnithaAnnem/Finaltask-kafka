c5ea60d52a0b10ec52bdf7511595f01d494028dc
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-18 12:41:20 +0530

    Update KafkaDeployment.groovy

--- KafkaSharedLib/src/org/cloudninja/kafka/template/KafkaDeployment.groovy
+++ KafkaSharedLib/src/org/cloudninja/kafka/template/KafkaDeployment.groovy
@@ -7,9 +7,6 @@ class KafkaDeployment implements Serializable {
         this.steps = steps
     }
 
-    // ----------------------------
-    // Git / Repo
-    // ----------------------------
     def checkoutCode(String repo, String branch) {
         steps.echo "Checking out Git repository..."
         steps.checkout([$class: 'GitSCM',
@@ -24,91 +21,51 @@ class KafkaDeployment implements Serializable {
 
     def dependencyScan() {
         steps.echo "Checking dependencies..."
-        steps.sh """
-            pip install safety && safety check -r requirements.txt || true
-            # Add Java/Maven scan if needed: mvn dependency-check:check
-        """
+        steps.sh "pip install safety && safety check -r requirements.txt || true"
     }
 
-    // ----------------------------
-    // Code-level
-    // ----------------------------
     def staticCodeAnalysis() {
         steps.echo "Running static code analysis..."
-        steps.sh "pylint **/*.py || true" // Python example
-        // For Java: steps.sh "mvn sonar:sonar"
+        steps.sh "pylint **/*.py || true"
     }
 
     def runUnitTests() {
         steps.echo "Running unit tests..."
-        steps.sh "pytest --maxfail=1 --disable-warnings -q || true" // Python example
-        // Java: steps.sh "mvn test"
+        steps.sh "pytest --maxfail=1 --disable-warnings -q || true"
     }
 
     def buildCode() {
         steps.echo "Compiling/building code..."
-        steps.sh "mvn clean compile || true" // Example for Java
+        steps.sh "mvn clean compile || true"
     }
 
     def codeCoverage() {
         steps.echo "Checking code coverage..."
         steps.sh "coverage run -m pytest && coverage report || true"
-        // Java: steps.sh "mvn jacoco:report"
     }
 
-    // ----------------------------
-    // Ansible-level
-    // ----------------------------
     def ansibleLint(String playbook) {
         steps.echo "Running ansible-lint on playbook..."
-        steps.sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-lint ${playbook}
-        """
+        steps.sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-lint ${playbook}"
     }
 
     def ansibleSyntaxCheck(String inventory, String playbook) {
         steps.echo "Checking Ansible playbook syntax..."
-        steps.sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-playbook -i ${inventory} ${playbook} --syntax-check
-        """
+        steps.sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${inventory} ${playbook} --syntax-check"
     }
 
     def ansibleDryRun(String inventory, String playbook, String user, String key) {
-        steps.echo "Performing dry run (check mode) on playbook..."
-        steps.sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-playbook -i ${inventory} ${playbook} --check -u ${user} --private-key ${key}
-        """
+        steps.echo "Performing dry run on playbook..."
+        steps.sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${inventory} ${playbook} --check -u ${user} --private-key ${key}"
     }
 
-    def ansibleIdempotencyTest(String inventory, String playbook, String user, String key) {
-        steps.echo "Checking Ansible idempotency..."
-        ansibleDryRun(inventory, playbook, user, key)
-    }
-
-    def inventoryPing(String inventory, String user, String key) {
-        steps.echo "Checking inventory reachability..."
-        steps.sh "ansible -i ${inventory} all -m ping -u ${user} --private-key ${key}"
-    }
-
-    // ----------------------------
-    // Deployment
-    // ----------------------------
     def deployZookeeper(String inventory, String playbook, String user, String key) {
         steps.echo "Deploying Zookeeper cluster..."
-        steps.sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags zookeeper
-        """
+        steps.sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags zookeeper"
     }
 
     def deployKafka(String inventory, String playbook, String user, String key) {
         steps.echo "Deploying Kafka cluster..."
-        steps.sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags kafka
-        """
+        steps.sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${inventory} ${playbook} -u ${user} --private-key ${key} --tags kafka"
     }
 }
